2ea5c097229eaeb00e719dc13361c65f29122a97
minhj0604 <minhyo0311@gmail.com>
2026-05-20 15:38:15 +0900

    [Feat] 프로젝트 생성 및 초기 세팅

---
+++ 2026_1A_MHJ_GameEngineIntro_TopDown/Assets/Script/GameDataManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[Serializable]
+public class PlayerData
+{
+    public List<string> collectedItems = new List<string>();
+    public int stage = 1;
+}
+public class GameDataManager : MonoBehaviour
+{
+    public static GameDataManager Instance;
+    public PlayerData playerData;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
